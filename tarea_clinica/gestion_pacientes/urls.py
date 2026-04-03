5fa00d2abdff15c3a788e2718f502b4eaf0388f9
SebastianToledoS <sebtoledosarmiento@gmail.com>
2026-04-02 21:29:14 -0300

    Update urls.py

--- tarea_clinica/gestion_pacientes/urls.py
+++ tarea_clinica/gestion_pacientes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.lista_pacientes, name='lista_pacientes'),
     path('pacientes/', views.lista_pacientes, name='lista_pacientes'),
     path('pacientes/nuevo/', views.crear_paciente, name='crear_paciente'),
 
